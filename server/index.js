a74c842a1918de1e2be0f37348d73dfe331807a8
drkspark <madhusudan753@gmail.com>
2023-09-02 16:13:33 +0530

    Added message text

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const {PORT} = require("./config/serverConfig");
-
-const {sendBasicEmail} = require("./mailer")
-
+const express = require("express");
+const cors = require("cors");
+const { PORT } = require("./config/serverConfig");
+const { sendBasicEmail } = require("./mailer");
+const generateStatement = require("./generateStatement");
 const app = express();
 
 app.use(express.json());
@@ -11,19 +10,18 @@ app.use(express.json());
 // Configure CORS for development (adjust as needed)
 app.use(
   cors({
-    origin: 'http://localhost:3000', 
-    methods: 'POST'
+    origin: "http://localhost:3000",
+    methods: "POST",
   })
 );
-
-
+const letterContent = generateStatement({});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   sendBasicEmail(
-          'sender',
-          'madhusudan753@gmail.com',
-          'Testing mailing service',
-          'Hello this is a test mail sent to u by node mailer'
-      );
+    "sender",
+    "20r01a6740@cmritonline.ac.in",
+    "Testing mailing service",
+    letterContent
+  );
 });
